1dfceaa55df7d817c0466219e761fa4bd9717466
Sai-kurapati4 <saiganeshkurapati@gmail.com>
2026-01-02 09:30:16 +0530

    Add files via upload

---
+++ conct_str.java
@@ -0,0 +1,6 @@
+public class conct_str{
+public static void main(String[] arg){
+String str1="sai";
+String str2="ganesh";
+String concate_str3=str1.concat(str2);
+System.out.println(concate_str3);}}
